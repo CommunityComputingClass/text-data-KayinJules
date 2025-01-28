37b3fd26e1487b402fdd219d4443e17fdd46603d
KayinJules <127335243+KayinJules@users.noreply.github.com>
2025-01-28 23:23:41 +0000

    card

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,20 @@
+let myMessage = "greetings!!!!!!!!!"
+let xLocation = 30
+
+let myFont;
+function preload(){
+  myFont = loadFont("ATHEROSSER.ttf")
+}
 function setup() {
   createCanvas(400, 400);
-}
 
+ 
+}
 function draw() {
-  background(200);
-  
+  background(50);
+  noStroke();
+  fill(255);
+  textFont(myFont)
+  text(myMessage, xLocation, 200), 50, 200;
+  xLocation = xLocation + 1
 }
